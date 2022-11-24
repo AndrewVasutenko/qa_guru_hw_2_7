d7f39d3e402e7d599683978033faabf9a0ab66ba
AndrewVasutenko <vasutenko@gmail.com>
2022-11-24 14:27:16 +0200

    Fixed bugs: 1: Add Test_ in function 2. Add selenium in requirements 3. Writing mistakes on 24,28,29 lines

--- Tests/test_demoqa_practice_form.py
+++ Tests/test_demoqa_practice_form.py
@@ -2,7 +2,7 @@ from Models import controls
 from Models import pages
 from selene.support.conditions import have
 
-def demo_qa_form(open_and_close_form):
+def test_demo_qa_form(open_and_close_form):
     pages.fill_fullname("Andrew", "Vasutenko")
     pages.fill_user_email("qqqqq@qqqq.qq")
     controls.select_gender_male()
@@ -21,9 +21,9 @@ def demo_qa_form(open_and_close_form):
     pages.table_result.should(have.text("Male"))
     pages.table_result.should(have.text("8800555353"))
     pages.table_result.should(have.text("19 April,1993"))
-    pages.table_result.should(have.text("Art", "Commerce"))
+    pages.table_result.should(have.text("Arts, Commerce"))
     pages.table_result.should(have.text("Music"))
     pages.table_result.should(have.text("meme.jpg"))
     pages.table_result.should(have.text("Russia,Moscow"))
-    pages.table_result.should(have.text("rajasthan"))
-    pages.table_result.should(have.text("jaipur"))
+    pages.table_result.should(have.text("Rajasthan"))
+    pages.table_result.should(have.text("Jaipur"))
